20e2cbb078229d361472b1763fe5f4290e7f3c1a
Kunal <majikunal7364@gmail.com>
2023-07-13 03:26:39 +0530

    added-home-content

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-
 @NgModule({
   declarations: [
     AppComponent,
